9e2427b61cc2e65273b3acf24a71774f6dca2aed
lizdmukami <lizdmukami@gmail.com>
2023-02-09 01:40:18 +0300

    reverse word method

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,28 @@
-# Write your code here
+# Given:    "Hello there, and how are you?"
+# Then:      reverse_each_word("Hello there, and how are you?")    
+# Output:    #=> "olleH ,ereht dna woh era ?uoy"
+
+# Pseudocode
+# Split the string on " " to get access to each word in the sentence
+# Reverse each word, and add it to a new array
+# Join the array of words on " " to create one string
+
+require 'pry'
+
+def reverse_each_word(sentence)
+    # Split the string on " " to get access to each word in the sentence
+    words = sentence.split
+
+    # Reverse each word, and add it to a new array
+    # Create a new array
+    reversed_words = []
+
+    # Iterate over the array of words
+    words.each do |word|
+        reversed_words << word.reverse
+    end
+    # Join the array of words on " " to create one string
+    reversed_words.join(" ")
+end
+
+puts reverse_each_word("Hello there, and how are you?")
